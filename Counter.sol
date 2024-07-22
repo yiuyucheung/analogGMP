aec38062acba20f48f8568b3bd37ac5bd3c0311e
cheung <32758831+yiuyucheung@users.noreply.github.com>
2024-07-22 16:48:12 +0800

    Update Counter.sol

--- Counter.sol
+++ Counter.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 import {IGmpReceiver} from "contracts/IGmpReceiver.sol";
-
+//gateway implement address 0x000000007f56768dE3133034FA730a909003a165
+//counter gateway address 0xB5D83c2436Ad54046d57Cd48c00D619D702F3814
 contract Counter is IGmpReceiver {
     address private immutable _gateway;
     uint256 public number;
